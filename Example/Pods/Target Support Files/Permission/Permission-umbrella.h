0065e41df4a109efe18b22acf047a2caf81e8764
Jameson-zxm <Morenotepad@163.com>
2016-12-09 10:49:51 +0800

    update

--- Example/Pods/Target Support Files/Permission/Permission-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double PermissionVersionNumber;
-FOUNDATION_EXPORT const unsigned char PermissionVersionString[];
-
--- Example/Pods/Target Support Files/RxPermission/RxPermission-umbrella.h
+++
@@ -1,8 +0,0 @@
-#ifdef __OBJC__
-#import <UIKit/UIKit.h>
-#endif
-
-
-FOUNDATION_EXPORT double RxPermissionVersionNumber;
-FOUNDATION_EXPORT const unsigned char RxPermissionVersionString[];
-
